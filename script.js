3d65ef4396ba7c7b0cb41e78fabc7aa0fcc98821
ccadarn <ccadarn@gmail.com>
2024-06-05 22:43:44 +0200

    Updated code found on interned generating radom color circles

---
+++ script.js
@@ -0,0 +1,37 @@
+const btn = document.querySelector("button");
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+document.addEventListener("DOMContentLoaded", () => {
+  canvas.width = document.documentElement.clientWidth;
+  canvas.height = document.documentElement.clientHeight;
+});
+
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+function random(number) {
+  return Math.floor(Math.random() * number);
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < 10000; i++) {
+    ctx.beginPath();
+    ctx.fillStyle = getRandomColor();
+    ctx.arc(
+      random(canvas.width),
+      random(canvas.height),
+      random(50),
+      0,
+      2 * Math.PI,
+    );
+    ctx.fill();
+  }
+}
+
+btn.addEventListener("click", draw);
